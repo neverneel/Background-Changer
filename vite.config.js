242d0515a3e779dd0f9b9cab46371625676996b3
NEEL <neelkamalpunj@gmail.com>
2026-01-28 23:59:09 +0530

    edited

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Background-Changer",
+  base: "./Background-Changer",
   plugins: [react(), tailwindcss()],
 })
